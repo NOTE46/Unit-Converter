038d9d0dd1b64b6245f57a17aa63275ef87f6af3
NOTE46 <sahilpaul1177@gmail.com>
2025-11-01 08:39:04 +0530

    update

--- Individual convertors/Temperature Converter.c
+++ Individual convertors/Temperature Converter.c
@@ -1,6 +1,11 @@
 
 #include <stdio.h>
 
+void error();
+void Celsius();
+void Fahrenheit();
+void Kelvin();
+
 int main()
 {
     char t;
@@ -15,152 +20,176 @@ int main()
 
         if (t != 'C' && t != 'F' && t != 'K' && t != 'c' && t != 'f' && t != 'k')
         {
-            printf("Error please enter correct key\n");
+            error();
         }
-    } while (t != 'C' && t != 'F' && t != 'K' && t != 'c' && t != 'f' && t != 'k');
+    }
+        while (t != 'C' && t != 'F' && t != 'K' && t != 'c' && t != 'f' && t != 'k')
+            ;
 
-    printf("You selected: %c\n", t);
+        printf("You selected: %c\n", t);
 
-    switch (t)
-    {
-    case 'C':
-    case 'c':
-    {
-        int tem;
-        do
+        switch (t)
         {
-            printf("Select input\nFahrenheit-1\nKelvin-2\nInput:");
-            scanf(" %d", &tem);
+        case 'C':
+        case 'c':
+        {
+            Celsius();
+             break;
+        }
+        case 'F':
+        case 'f':
+        {
+           Fahrenheit();
+           break;
+        }
+        case 'K':
+        case 'k':
+        {
+            Kelvin();
+            break;
+        }
+        }
 
-            if (tem != 1 && tem != 2)
+        return 0;
+    }
+
+void error()
+    {
+        printf("\nERROR, Enter right command\n");
+        while (getchar() != '\n')
+            ;
+    }
+
+void Celsius()
+{
+    int tem;
+            do
             {
-                printf("Error please enter correct key\n");
+                printf("Select input\nFahrenheit-1\nKelvin-2\nInput:");
+                scanf(" %d", &tem);
+
+                if (tem != 1 && tem != 2)
+                {
+                    error();
+                }
+
+            } while (tem != 1 && tem != 2);
+
+            switch (tem)
+            {
+            case 1:
+            {
+                float Fahrenheit;
+                printf("Enter Fahrenheit:");
+                scanf("%f", &Fahrenheit);
+
+                float c = (5.0 / 9.0) * (Fahrenheit - 32.0);
+
+                printf("%g Fahrenheit = %g Celsius", Fahrenheit, c);
+                break;
+            }
+            case 2:
+            {
+                float kelvin;
+                printf("Enter Kelvin:");
+                scanf("%f", &kelvin);
+
+                float k = kelvin - 273.15;
+
+                printf("%g Kelvin = %g Celsius", kelvin, k);
+                break;
+            }
+            }
+}
+
+void Fahrenheit()
+{
+ int tem;
+            do
+            {
+                printf("Select input\nCelsius-1\nKelvin-2\nInput:");
+                scanf(" %d", &tem);
+
+                if (tem != 1 && tem != 2)
+                {
+                    error();
+                }
+
+            } while (tem != 1 && tem != 2);
+
+            switch (tem)
+            {
+            case 1:
+            {
+                float Cel;
+                printf("Enter Celsius:");
+                scanf("%f", &Cel);
+
+                float f = (Cel * 9.0 / 5.0) + 32.0;
+
+                printf("%g Celsius = %g Fahrenheit", Cel, f);
+
+                break;
             }
 
-        } while (tem != 1 && tem != 2);
-
-        switch (tem)
-        {
-        case 1:
-        {
-            float Fahrenheit;
-            printf("Enter Fahrenheit:");
-            scanf("%f", &Fahrenheit);
-
-            float c = (5.0 / 9.0) * (Fahrenheit - 32.0);
-
-            printf("%g Fahrenheit = %g Celsius", Fahrenheit, c);
-            break;
-        }
-        case 2:
-        {
-            float kelvin;
-            printf("Enter Kelvin:");
-            scanf("%f", &kelvin);
-
-            float k = kelvin - 273.15;
-
-            printf("%g Kelvin = %g Celsius", kelvin, k);
-            break;
-        }
-        }
-        break;
-    }
-    case 'F':
-    case 'f':
-    {
-        int tem;
-        do
-        {
-            printf("Select input\nCelsius-1\nKelvin-2\nInput:");
-            scanf(" %d", &tem);
-
-            if (tem != 1 && tem != 2)
+            case 2:
             {
-                printf("Error please enter correct key\n");
+                float Kelvin;
+                printf("Enter Kelvin:");
+                scanf("%f", &Kelvin);
+
+                float C = Kelvin - 273.15;
+
+                float fi = (C * 9.0 / 5.0) + 32.0;
+
+                printf("%g Kelvin = %g Fahrenheit", Kelvin, fi);
+
+                break;
+            }
+            }
+           }
+
+void Kelvin()
+{
+ int tem;
+            do
+            {
+                printf("Select input\nCelsius-1\nFahrenheit-2\nInput:");
+                scanf(" %d", &tem);
+
+                if (tem != 1 && tem != 2)
+                {
+                    error();
+                }
+
+            } while (tem != 1 && tem != 2);
+
+            switch (tem)
+            {
+            case 1:
+            {
+                float Cel;
+                printf("Enter Celsius:");
+                scanf("%f", &Cel);
+
+                float k = Cel + 273.15;
+
+                printf("%g Celsius = %g Kelvin", Cel, k);
+
+                break;
             }
 
-        } while (tem != 1 && tem != 2);
-
-        switch (tem)
-        {
-        case 1:
-        {
-            float Cel;
-            printf("Enter Celsius:");
-            scanf("%f", &Cel);
-
-            float f = (Cel * 9.0 / 5.0) + 32.0;
-
-            printf("%g Celsius = %g Fahrenheit", Cel, f);
-
-            break;
-        }
-
-        case 2:
-        {
-            float Kelvin;
-            printf("Enter Kelvin:");
-            scanf("%f", &Kelvin);
-
-            float C = Kelvin - 273.15;
-
-            float fi = (C * 9.0 / 5.0) + 32.0;
-
-            printf("%g Kelvin = %g Fahrenheit", Kelvin, fi);
-
-            break;
-        }
-        }
-        break;
-    }
-    case 'K':
-    case 'k':
-    {
-        int tem;
-        do
-        {
-            printf("Select input\nCelsius-1\nFahrenheit-2\nInput:");
-            scanf(" %d", &tem);
-
-            if (tem != 1 && tem != 2)
+            case 2:
             {
-                printf("Error please enter correct key\n");
+                float Fahrenheit;
+                printf("Enter Fahrenheit:");
+                scanf("%f", &Fahrenheit);
+
+                float k = ((Fahrenheit - 32.0) * 5.0) / 9.0 + 273.15;
+
+                printf("%g Fahrenheit = %g Kelvin", Fahrenheit, k);
+
+                break;
             }
-
-        } while (tem != 1 && tem != 2);
-
-        switch (tem)
-        {
-        case 1:
-        {
-            float Cel;
-            printf("Enter Celsius:");
-            scanf("%f", &Cel);
-
-            float k = Cel + 273.15;
-
-            printf("%g Celsius = %g Kelvin", Cel, k);
-
-            break;
-        }
-
-        case 2:
-        {
-            float Fahrenheit;
-            printf("Enter Fahrenheit:");
-            scanf("%f", &Fahrenheit);
-
-            float k = ((Fahrenheit - 32.0) * 5.0) / 9.0 + 273.15;
-
-            printf("%g Fahrenheit = %g Kelvin", Fahrenheit, k);
-
-            break;
-        }
-        }
-        break;
-    }
-    }
-
-    return 0;
+            }
 }
